11ce9c2ccdaebd10b86e5f21cec728ec20d98f8a
ties-mitsuhashi <mitsuhashi@cccties.org>
2015-06-24 10:23:34 +0900

    書き出し用バッジファイル・スクリプトの更新

--- run-html.sh
+++ run-html.sh
@@ -1,6 +1,59 @@
 #!/bin/sh
-if [ X$2 != X ]; then
-java -jar chilo-epub3-maker.jar -publish html -2 -course $1  -input-path ./ -output-path ./ -output-name $2
+
+usage()
+{
+	echo "usage: $0 -c <course name> [ -i <input dir> -o <output_dir> -f <output_name> ]"
+	exit 0
+}
+
+
+### parse command line options
+RUNMODE=real
+while getopts dc:i:o:f: OPT
+do
+        case $OPT in
+	"d" )
+		RUNMODE=dry
+		;;
+	"c" )  
+		COURSE=$OPTARG
+		;;
+	"i" )  
+		INPUT_DIR=$OPTARG
+		;;
+	"o" )  
+		OUTPUT_DIR=$OPTARG
+		;;
+	"f" )
+		OUTPUT_NAME=$OPTARG
+		;;
+	"*" )
+		usage
+		;;
+	esac
+done
+
+### check parameters
+
+if [ X${COURSE} = X ]; then
+	echo "no course specified."
+	usage
+fi
+
+if [ X${INPUT_DIR} = X ]; then
+	INPUT_DIR=./
+fi
+
+if [ X${OUTPUT_DIR} = X ]; then
+	OUTPUT_DIR=./
+fi
+
+if [ X${OUTPUT_NAME} != X ]; then
+	ONAME_OPT="-output-name ${OUTPUT_NAME}"
+fi
+
+if [ X${RUNMODE} = Xreal ]; then
+	java -jar ./chilo-epub3-maker.jar -publish html -course ${COURSE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}
 else
-java -jar chilo-epub3-maker.jar -publish html -2 -course $1  -input-path ./ -output-path ./ 
+	echo "DRY-RUN: java -jar ./chilo-epub3-maker.jar -publish html -course ${COURSE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}"
 fi
--- run.bat
+++ run.bat
@@ -1,7 +1,67 @@
 @echo off
 
-if "%2"=="" (
-          java -jar chilo-epub3-maker.jar -course %1  -input-path ./ -output-path ./
+set PROG=%0
+
+call :CLEARVALS
+
+:GETOPTS
+	if /I "%1" == "" goto usage
+	if /I "%1" == "-h" goto usage
+	if /I "%1" == "-d" call :DRYRUN
+	if /I "%1" == "-c" call :SETCOURSE %2; shift
+	if /I "%1" == "-i" call :SETINPUTDIR %2; shift
+	if /I "%1" == "-o" call :SETOUTPUTDIR %2; shift
+	if /I "%1" == "-f" call :SETOUTPUTNAME %2; shift
+	shift
+if not (%1)==() goto GETOPTS
+
+goto MAIN
+
+:usage
+	echo "%PROG% -c <course name> [ -i <input dir> -o <output_dir> -f <output_name> ]"
+	goto ALLDONE
+
+:CLEARVALS
+	set RUNMODE=
+	set COURSE=
+	set DRYRUN=
+	set SETCOURSE=
+	set SETINPUTDIR=
+	set SETOUTPUTDIR=
+	set SETOUTPUTNAME=
+	exit /b
+
+:DRYRUN
+	set RUNMODE=dry
+	exit /b
+
+:SETCOURSE
+	set COURSE=%1
+	exit /b
+
+:SETINPUTDIR
+	set INPUTDIR=-input-path %1
+	exit /b
+
+:SETOUTPUTDIR
+	set OUTPUTDIR=-output-path %1
+	exit /b
+
+:SETOUTPUTNAME
+	set OUTPUTNAME=-output-name %1
+	exit /b
+
+:MAIN
+
+if "%COURSE%"=="" goto usage
+
+
+if "%RUNMODE%"=="dry" (
+	echo "dry-run:java -jar chilo-epub3-maker.jar -c %COURSE% %INPUTDIR% %OUTPUTDIR% %OUTPUTNAME%"
 ) else (
-          java -jar chilo-epub3-maker.jar -course %1  -input-path ./ -output-path ./ -output-name %2
+	java -jar chilo-epub3-maker.jar -course %COURSE% %INPUTDIR% %OUTPUTDIR% %OUTPUTNAME%
 )
+
+:ALLDONE
+	call :CLEARVALS
+
