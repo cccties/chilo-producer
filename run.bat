137e9b616e6fb9798d48ecc1a7dc203131f3b2c0
ties-mitsuhashi <mitsuhashi@cccties.org>
2015-09-16 14:38:12 +0900

    run.bat/shを更新

--- run.bat
+++ run.bat
@@ -10,6 +10,7 @@ call :CLEARVALS
 	if /I "%1" == "-d" call :DRYRUN
 	if /I "%1" == "-c" call :SETCOURSE %2; shift
 	if /I "%1" == "-i" call :SETINPUTDIR %2; shift
+	if /I "%1" == "-t" call :SETOUTPUTTYPE %2; shift
 	if /I "%1" == "-o" call :SETOUTPUTDIR %2; shift
 	if /I "%1" == "-f" call :SETOUTPUTNAME %2; shift
 	shift
@@ -18,7 +19,7 @@ if not (%1)==() goto GETOPTS
 goto MAIN
 
 :usage
-	echo "%PROG% -c <name of your folder> [ -i <input dir> -o <output_dir> -f <output_name> ]"
+	echo "%PROG% -c <name of your folder> -t <name of book type> [ -i <input dir> -o <output_dir> -f <output_name> ]"
 	goto ALLDONE
 
 :CLEARVALS
@@ -27,6 +28,7 @@ goto MAIN
 	set DRYRUN=
 	set COURSE=
 	set INPUTDIR=
+	set OUTPUTTYPE=
 	set OUTPUTDIR=
 	set OUTPUTNAME=
 	exit /b
@@ -41,6 +43,10 @@ goto MAIN
 
 :SETINPUTDIR
 	set INPUTDIR=-input-path %1
+	exit /b
+
+:SETOUTPUTTYPE
+	set OUTPUTTYPE=%1
 	exit /b
 
 :SETOUTPUTDIR
@@ -59,7 +65,23 @@ if "%COURSE%"=="" goto usage
 if "%RUNMODE%"=="dry" (
 	echo "dry-run:java -jar chilo-epub3-maker.jar -c %COURSE% %INPUTDIR% %OUTPUTDIR% %OUTPUTNAME%"
 ) else (
-	java -jar chilo-epub3-maker.jar -course %COURSE% %INPUTDIR% %OUTPUTDIR% %OUTPUTNAME%
+	if "%OUTPUTTYPE%"=="epub" (
+		xcopy page_templates\templates_epub chiloPro\common\templates  /D /S /R /Y /I /K
+		java -jar chilo-epub3-maker.jar -course %COURSE% %INPUTDIR% %OUTPUTDIR% %OUTPUTNAME%
+		rd /s /q chiloPro\common\templates
+	) else if "%OUTPUTTYPE%"=="ext" (
+		xcopy page_templates\templates_ext chiloPro\common\templates  /D /S /R /Y /I /K
+		java -jar chilo-epub3-maker.jar -course %COURSE% %INPUTDIR% %OUTPUTDIR% %OUTPUTNAME%
+		rd /s /q chiloPro\common\templates
+	) else if "%OUTPUTTYPE%"=="web" (
+		xcopy page_templates\templates_epub chiloPro\common\templates  /D /S /R /Y /I /K
+		xcopy page_templates\templates_web chiloPro\common\web-templates  /D /S /R /Y /I /K
+		java -jar chilo-epub3-maker.jar -publish html -course %COURSE% %INPUTDIR% %OUTPUTDIR% %OUTPUTNAME%
+		rd /s /q chiloPro\common\templates
+		rd /s /q chiloPro\common\web-templates
+	) else (
+		goto usage
+	)
 )
 
 :ALLDONE
--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 usage()
 {
-	echo "usage: $0 -c <name of your folder> [ -i <input dir> -o <output_dir> -f <output_name> ]"
+	echo "usage: $0 -c <name of your folder> -t <name of book type> [ -i <input dir> -o <output_dir> -f <output_name> ]"
 	exit 0
 }
 
 
 ### parse command line options
 RUNMODE=real
-while getopts dc:i:o:f: OPT
+while getopts dc:t:i:o:f: OPT
 do
         case $OPT in
 	"d" )
@@ -20,6 +20,9 @@ do
 		;;
 	"i" )  
 		INPUT_DIR=$OPTARG
+		;;
+	"t" )  
+		OUTPUT_TYPE=$OPTARG
 		;;
 	"o" )  
 		OUTPUT_DIR=$OPTARG
@@ -40,6 +43,11 @@ if [ X${COURSE} = X ]; then
 	usage
 fi
 
+if [ X${OUTPUT_TYPE} = X ]; then
+	echo "no book type specified."
+	usage
+fi
+
 if [ X${INPUT_DIR} = X ]; then
 	INPUT_DIR=./
 fi
@@ -53,7 +61,23 @@ if [ X${OUTPUT_NAME} != X ]; then
 fi
 
 if [ X${RUNMODE} = Xreal ]; then
-	java -jar ./chilo-epub3-maker.jar -course ${COURSE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}
+	if [ X${OUTPUT_TYPE} = Xepub ]; then
+		cp -r ./page_templates/templates_epub ./chiloPro/common/templates
+		java -jar ./chilo-epub3-maker.jar -course ${COURSE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}
+		rm -rf ./chiloPro/common/templates
+	elif [ X${OUTPUT_TYPE} = Xext ]; then
+		cp -r ./page_templates/templates_ext ./chiloPro/common/templates
+		java -jar ./chilo-epub3-maker.jar -course ${COURSE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}
+		rm -rf ./chiloPro/common/templates
+	elif [ X${OUTPUT_TYPE} = Xweb ]; then
+		cp -r ./page_templates/templates_epub ./chiloPro/common/templates
+		cp -r ./page_templates/templates_web ./chiloPro/common/web-templates
+		java -jar ./chilo-epub3-maker.jar -publish html -course ${COURSE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}
+		rm -rf ./chiloPro/common/templates
+		rm -rf ./chiloPro/common/web-templates
+	else
+		usage
+	fi
 else
 	echo "DRY-RUN: java -jar ./chilo-epub3-maker.jar -course ${COURSE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}"
 fi
