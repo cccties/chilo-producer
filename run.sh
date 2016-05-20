b868d51d7f7d59112a7cf88f71cd6bdbd5c476d3
ties-mitsuhashi <mitsuhashi@cccties.org>
2016-05-20 15:21:27 +0900

    Update run.sh
    * Disable output of web version CHiLO Book
    * Add option to select book template in the command to output CHiLO Book

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 usage()
 {
-	echo "usage: $0 -c <name of your folder> -t <book type> [ -i <input dir> -o <output_dir> -f <output_name> ]"
+	echo "usage: $0 -c <name of your directory> -s <name of the applied book template> [ -i <input dir> -o <output_dir> -f <output_name> ]"
 	exit 0
 }
 
 
 ### parse command line options
 RUNMODE=real
-while getopts dc:t:i:o:f: OPT
+while getopts dc:s:i:o:f: OPT
 do
         case $OPT in
 	"d" )
@@ -21,8 +21,8 @@ do
 	"i" )  
 		INPUT_DIR=$OPTARG
 		;;
-	"t" )  
-		OUTPUT_TYPE=$OPTARG
+	"s" )  
+		BOOK_TEMPLATE=$OPTARG
 		;;
 	"o" )  
 		OUTPUT_DIR=$OPTARG
@@ -43,8 +43,8 @@ if [ X${COURSE} = X ]; then
 	usage
 fi
 
-if [ X${OUTPUT_TYPE} = X ]; then
-	echo "no book type specified."
+if [ X${BOOK_TEMPLATE} = X ]; then
+	echo "no template name specified."
 	usage
 fi
 
@@ -60,35 +60,23 @@ if [ X${OUTPUT_NAME} != X ]; then
 	ONAME_OPT="-output-name ${OUTPUT_NAME}"
 fi
 
-if [ X${RUNMODE} = Xreal ]; then
-	if [ X${OUTPUT_TYPE} = Xepub ]; then
-		cp -r ./page_templates/templates_epub ./chiloPro/common/templates
+
+if [ X${RUNMODE} = Xdry ]; then
+		echo "DRY-RUN: java -jar ./chilo-epub3-maker.jar -course ${COURSE} -type ${BOOK_TEMPLATE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}"
+		
+elif [ X${RUNMODE} = Xreal ]; then
+#	if [ X${BOOK_TEMPLATE} = Xepub ]; then
+		COURSEBASE_DIR=`sed -n '/CourseBaseDir/p' chilo-epub3-maker.xml | sed -e 's/<[^>]*>//g' | tr -d '\r' | tr -d '\n'`
+		cp -R ./book_templates/${BOOK_TEMPLATE}/page_templates ./${COURSEBASE_DIR}/common/templates
+		cp -R ./book_templates/${BOOK_TEMPLATE}/styles ./${COURSEBASE_DIR}/${COURSE}/${INPUT_DIR}/common/
+		cp -R ./book_templates/${BOOK_TEMPLATE}/images ./${COURSEBASE_DIR}/common/
+		
 		java -jar ./chilo-epub3-maker.jar -course ${COURSE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}
-		rm -rf ./chiloPro/common/templates
-	elif [ X${OUTPUT_TYPE} = Xweb ]; then
-		cp -r ./page_templates/templates_epub ./chiloPro/common/templates
-		cp -r ./page_templates/templates_web ./chiloPro/common/web-templates
-		mkdir ./escape
-
-		COURSEBASE_DIR=`sed -n '/CourseBaseDir/p' chilo-epub3-maker.xml | sed -e 's/<[^>]*>//g'`
-		FONT_DIR=$(cd $COURSEBASE_DIR/$COURSE && cd $INPUT_DIR/common && pwd)
-
-		if [ -e $FONT_DIR/fonts ]; then
-			mv $FONT_DIR/fonts ./escape/
-		fi
-
-		java -jar ./chilo-epub3-maker.jar -publish html -course ${COURSE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}
-		rm -rf ./chiloPro/common/templates
-		rm -rf ./chiloPro/common/web-templates
-
-		if [ -e ./escape/fonts ]; then
-			mv ./escape/fonts $FONT_DIR 
-		fi
-
-		rm -rf ./escape
-	else
-		usage
-	fi
+		
+		rm -rf ./${COURSEBASE_DIR}/common/templates
+		rm -rf ./${COURSEBASE_DIR}/${COURSE}/${INPUT_DIR}/common/styles
+		rm -rf ./${COURSEBASE_DIR}/common/images
+		
 else
-	echo "DRY-RUN: java -jar ./chilo-epub3-maker.jar -course ${COURSE} -type ${OUTPUT_TYPE} -input-path ${INPUT_DIR} -output-path ${OUTPUT_DIR} ${ONAME_OPT}"
+	usage
 fi
